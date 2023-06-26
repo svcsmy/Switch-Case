e274dece5e069fcd60e580e8e367d98ea190c493
Sümeyye <58435258+svcsmy@users.noreply.github.com>
2023-06-26 18:11:41 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class main {
+
+ public static void main(String[] args) {
+	
+	 Scanner scanner = new Scanner(System.in);
+	 
+	 System.out.println("Please enter a transaction");
+	 int process = scanner.nextInt();
+	
+	 switch(process) {
+	  
+	     case 1:
+	    	 System.out.println("1. Process");
+	    	 break;
+	     case 2:
+	    	 System.out.println("2. Process");
+	    	 break;
+	     case 3:
+	    	 System.out.println("3. Process");
+	    	 break;
+	     case 4: 
+	    	 System.out.println("4. Process");
+	    	 break;
+	     default: 
+	    	 System.out.println("You entered an invalid transaction...");
+	    	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 }
+	 
+	 
+	
+ }
+}
